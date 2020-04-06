3d305ef1103da5364da10333991bade91562aa6c
Ben Honeywill <bhnywl@gmail.com>
2020-04-06 12:22:28 +0100

    npm audit & fixed font flash by using google font cdn

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,6 @@
 // CSS
 import "normalize.css";
 
-// Custom typefaces
-import "typeface-lato";
-import "typeface-lora";
-
 // App wrapper
 import App from "./src/app";
 
--- src/components/seo/seo.component.js
+++ src/components/seo/seo.component.js
@@ -21,6 +21,13 @@ const SEO = ({ description, lang, meta, keywords, title, image }) => {
       htmlAttributes={{ lang }}
       title={title}
       titleTemplate={`%s | ${siteMetadata.title}`}
+      link={[
+        {
+          href:
+            "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=Lora:ital,wght@0,400;0,700;1,400&display=swap",
+          rel: "stylesheet"
+        }
+      ]}
       meta={[
         {
           name: "description",
