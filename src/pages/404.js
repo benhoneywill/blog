e9de8d8f2c5a05db6c40bdd4ec83d89f3b839312
Ben Honeywill <bhnywl@gmail.com>
2020-11-03 00:05:41 +0000

    fix deprecation error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "gatsby";
+import { graphql } from "gatsby";
 
 // Components
 import Container from "../components/container";
