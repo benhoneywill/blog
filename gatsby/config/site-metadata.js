d8237062fa83330aafcb02d8fad0162ece904e42
Ben Honeywill <benhoneywill@gmail.com>
2022-03-13 22:54:02 +0000

    update site metadata

--- gatsby/config/site-metadata.js
+++ gatsby/config/site-metadata.js
@@ -1,8 +1,7 @@
 module.exports = {
   title: "Ben Honeywill",
   author: "Ben Honeywill",
-  description:
-    "I am Ben Honeywill. I'm a Front-End Engineer from Bournemouth, UK. Web development, JavaScript & more!",
+  description: "I am Ben Honeywill. I'm a Front-End Engineer from the UK.",
   siteUrl: "https://benhoneywill.com",
   social: {
     twitter: "benhoneywill",
